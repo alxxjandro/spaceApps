13e644ff8e8fe0496a87e1204df845a4ef2a6847
Myriam <A01569121@tec.mx>
2024-10-05 15:42:26 -0600

    nada

---
+++ components/Navbar.jsx
@@ -0,0 +1,14 @@
+export default function Navbar(){
+    return (
+        <div>
+            <div className="NavButtonsLeft">
+                <button>Button1</button>
+                <button>Button1</button>
+                <button>Button12s</button>
+            </div>
+            <div className="NavButtonsLeft">
+
+            </div>
+        </div>
+    )
+}
